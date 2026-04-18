606cdbba9491b50c744f6e1c3e91f299a7311442
richmondrockson <richmond.rockson@amalitech.org>
2026-04-18 01:30:46 +0000

    feat: establish firebase connection, add auth middleware and POST and GET complaint routes

--- server/index.js
+++ server/index.js
@@ -4,9 +4,14 @@ const PORT = 5000;
 
 app.use(express.json());
 
+const complaintRoutes = require("./routes/complaints");
+app.use("/complaints", complaintRoutes);
+
 app.get("/", (req, res) => {
   res.send("Hello from the hostel complaint app!");
 });
+
+const { db } = require("./config/firebase");
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
