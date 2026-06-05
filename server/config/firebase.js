3bfe7df48039b4e5683fa7f0e3d632faa1d865a8
richmondrockson <richmond.rockson@amalitech.org>
2026-06-05 20:25:21 +0000

    update firebase config to support environment variables for deployment

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,9 +1,20 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+let credential;
+
+if (process.env.FIREBASE_PRIVATE_KEY) {
+  credential = admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  });
+} else {
+  const serviceAccount = require("../serviceAccountKey.json");
+  credential = admin.credential.cert(serviceAccount);
+}
+
+admin.initializeApp({ credential });
 
 const db = admin.firestore();
 
