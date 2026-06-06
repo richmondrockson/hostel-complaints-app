f235a1430c2b90e8bd1e49569eb3930205fa6927
richmondrockson <richmond.rockson@amalitech.org>
2026-06-06 01:27:31 +0000

    fix: use base64 encoded private key for Render deployment

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -2,15 +2,14 @@ const admin = require("firebase-admin");
 
 let credential;
 
-if (process.env.FIREBASE_PRIVATE_KEY) {
+if (process.env.FIREBASE_PRIVATE_KEY_BASE64) {
   credential = admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n").replace(
-      /"/g,
-      "",
-    ),
+    privateKey: Buffer.from(
+      process.env.FIREBASE_PRIVATE_KEY_BASE64,
+      "base64",
+    ).toString("utf8"),
   });
 } else {
   const serviceAccount = require("../serviceAccountKey.json");
