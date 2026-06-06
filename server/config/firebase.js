833832d07aafc849cc7a43801ef7c2afc373023a
richmondrockson <richmond.rockson@amalitech.org>
2026-06-06 01:47:14 +0000

    revert to direct private key

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,22 +1,12 @@
 const admin = require("firebase-admin");
 
-let credential;
-
-if (process.env.FIREBASE_PRIVATE_KEY_BASE64) {
-  credential = admin.credential.cert({
+admin.initializeApp({
+  credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: Buffer.from(
-      process.env.FIREBASE_PRIVATE_KEY_BASE64,
-      "base64",
-    ).toString("utf8"),
-  });
-} else {
-  const serviceAccount = require("../serviceAccountKey.json");
-  credential = admin.credential.cert(serviceAccount);
-}
-
-admin.initializeApp({ credential });
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
+});
 
 const db = admin.firestore();
 
